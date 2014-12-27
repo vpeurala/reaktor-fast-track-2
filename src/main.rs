bce40262db56402d253971ca26c56e4397fe8d36
Ville Peurala <ville.peurala@gmail.com>
2014-12-27 14:26:02 +0200

    Infinite loop

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use std::collections::HashMap;
 use std::collections::HashSet;
 use std::collections::BinaryHeap;
 use std::io::File;
-use std::io::IoError;
 use serialize::json;
 
 type Label  = u16;
@@ -30,7 +29,7 @@ impl Graph {
     let mut visited: HashSet<Label> = HashSet::new();
     let mut frontier: BinaryHeap<Route> = BinaryHeap::new();
     visited.insert(from);
-    for edg in self.outgoing(from).iter() {
+    for edg in self.outgoing(from).unwrap().iter() {
       frontier.push(vec![*edg]);
     }
     loop {
@@ -38,23 +37,27 @@ impl Graph {
         None => panic!("No route found!"),
         Some(p) => p
       };
-      if (cheapest_route.end_label() == to) {
+      if cheapest_route.end_label() == to {
         let mut result: Vec<Label> = cheapest_route.iter().map(|edg| edg.0).collect();
         result.insert(0, from);
         return result;
       } else {
-        let frontier_expansion = self.outgoing(cheapest_route.end_label());
-        for &fe in frontier_expansion.iter() {
-          let mut cheapest_route_clone = cheapest_route.clone();
-          cheapest_route_clone.push(fe);
-          frontier.push(cheapest_route_clone);
+        self.outgoing(cheapest_route.end_label());
+        match self.outgoing(cheapest_route.end_label()) {
+          None => (),
+          Some (frontier_expansion) => for &fe in frontier_expansion.iter() {
+            let mut cheapest_route_clone = cheapest_route.clone();
+            cheapest_route_clone.push(fe);
+            frontier.push(cheapest_route_clone);
+          }
         }
       }
     }
   }
 
-  fn outgoing(&self, from: Label) -> &Vec<Edge> {
-    self.vertices.get(&from).unwrap()
+  fn outgoing(&self, from: Label) -> Option<&Vec<Edge>> {
+    print!("outgoing: {}\n", from);
+    self.vertices.get(&from)
   }
 }
 
@@ -107,8 +110,8 @@ fn make_graph(v: &Vec<JsonEdge>) -> Graph {
 #[test]
 fn test_outgoing() {
   let graph = graph_from_json_file("graph.json");
-  assert_eq!(vec![(4384u16, 15u16)], *graph.outgoing(3138));
-  assert_eq!(vec![(6784u16, 2u16), (5069u16, 14u16), (4049u16, 14u16)], *graph.outgoing(3144));
+  assert_eq!(vec![(4384u16, 15u16)], *graph.outgoing(3138).unwrap());
+  assert_eq!(vec![(6784u16, 2u16), (5069u16, 14u16), (4049u16, 14u16)], *graph.outgoing(3144).unwrap());
 }
 
 #[test]
