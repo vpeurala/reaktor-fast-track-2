65f0ea4b1260f351bc02d6a01482eeedffa3dd9e
Ville Peurala <ville.peurala@gmail.com>
2017-10-31 12:18:10 +0200

    WIP. Trying to get this to compile with Rust 1.21.0, but not there yet.

--- src/graph.rs
+++ src/graph.rs
@@ -1,136 +1,135 @@
-extern crate collections;
-
+use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::collections::BinaryHeap;
 
-pub type Label  = uint;
-pub type Weight = uint;
-pub type Edge   = (Label, Weight);
+pub type Label = u32;
+pub type Weight = u32;
+pub type Edge = (Label, Weight);
 
-#[deriving(Eq, PartialEq, Show)]
+#[derive(Eq, PartialEq, Debug)]
 pub struct Route {
-  start_label: Label,
-  edges:       Vec<Edge>
+    start_label: Label,
+    edges: Vec<Edge>
 }
 
 pub trait WeightedDirectedGraph {
-  fn dijkstra(&self, from: Label, to: Label) -> Option<Route> {
-    let mut visited: HashSet<Label> = HashSet::new();
-    let mut frontier: BinaryHeap<Route> = BinaryHeap::new();
-    visited.insert(from);
-    for edg in self.outgoing_unvisited(from, &visited).unwrap_or_default().iter() {
-      frontier.push(Route { start_label: from, edges: vec![*edg] });
-    }
-    loop {
-      let cheapest_route = match frontier.pop() {
-        None    => return None,
-        Some(p) => p
-      };
-      if cheapest_route.end_label() == to {
-        return Some(cheapest_route)
-      } else {
-        visited.insert(cheapest_route.end_label());
-        match self.outgoing_unvisited(cheapest_route.end_label(), &visited) {
-          None => (),
-          Some (frontier_expansion) => for &fe in frontier_expansion.iter() {
-            let mut cheapest_route_clone = cheapest_route.edges.clone();
-            cheapest_route_clone.push(fe);
-            frontier.push(Route { start_label: from, edges: cheapest_route_clone });
-          }
+    fn dijkstra(&self, from: Label, to: Label) -> Option<Route> {
+        let mut visited: HashSet<Label> = HashSet::new();
+        let mut frontier: BinaryHeap<Route> = BinaryHeap::new();
+        visited.insert(from);
+        for edg in self.outgoing_unvisited(from, &visited).unwrap_or_default().iter() {
+            frontier.push(Route { start_label: from, edges: vec![*edg] });
         }
-      }
+        loop {
+            let cheapest_route = match frontier.pop() {
+                None => return None,
+                Some(p) => p
+            };
+            if cheapest_route.end_label() == to {
+                return Some(cheapest_route);
+            } else {
+                visited.insert(cheapest_route.end_label());
+                match self.outgoing_unvisited(cheapest_route.end_label(), &visited) {
+                    None => (),
+                    Some(frontier_expansion) => for &fe in frontier_expansion.iter() {
+                        let mut cheapest_route_clone = cheapest_route.edges.clone();
+                        cheapest_route_clone.push(fe);
+                        frontier.push(Route { start_label: from, edges: cheapest_route_clone });
+                    }
+                }
+            }
+        }
     }
-  }
 
-  fn outgoing_unvisited(&self, from: Label, visited: &HashSet<Label>) -> Option<Vec<Edge>> {
-    match self.outgoing(from) {
-      None => None,
-      Some(all_outgoing) => {
-        let output: Vec<Edge> = all_outgoing.iter().filter(|e| !visited.contains(&e.0)).map(|e| e.clone()).collect();
-        Some(output)
-      }
+    fn outgoing_unvisited(&self, from: Label, visited: &HashSet<Label>) -> Option<Vec<Edge>> {
+        match self.outgoing(from) {
+            None => None,
+            Some(all_outgoing) => {
+                let output: Vec<Edge> = all_outgoing.iter().filter(|e| !visited.contains(&e.0)).map(|e| e.clone()).collect();
+                Some(output)
+            }
+        }
     }
-  }
 
-  fn outgoing(&self, from: Label) -> Option<&Vec<Edge>>;
+    fn outgoing(&self, from: Label) -> Option<&Vec<Edge>>;
 }
 
 trait Weighted {
-  fn weight(&self) -> Weight;
+    fn weight(&self) -> Weight;
 }
 
 impl Weighted for Route {
-  fn weight(&self) -> Weight {
-    self.edges.iter().map(|e| e.weight()).fold(0, |sum, weight| sum + weight)
-  }
+    fn weight(&self) -> Weight {
+        self.edges.iter().map(|e| e.weight()).fold(0, |sum, weight| sum + weight)
+    }
 }
 
 impl Weighted for Edge {
-  fn weight(&self) -> Weight {
-    self.1
-  }
+    fn weight(&self) -> Weight {
+        self.1
+    }
 }
 
 impl Ord for Route {
-  fn cmp(&self, other: &Route) -> Ordering {
-    // Flipped around to get min-heap instead of max-heap.
-    other.weight().cmp(&self.weight())
-  }
+    fn cmp(&self, other: &Route) -> Ordering {
+        // Flipped around to get min-heap instead of max-heap.
+        other.weight().cmp(&self.weight())
+    }
 }
 
 impl PartialOrd for Route {
-  fn partial_cmp(&self, other: &Route) -> Option<Ordering> {
-    Some(self.cmp(other))
-  }
+    fn partial_cmp(&self, other: &Route) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
 }
 
 impl Route {
-  pub fn label_vec(&self) -> Vec<Label> {
-    let mut edge_labels: Vec<Label> = self.edges.iter().map(|e| e.0).collect();
-    edge_labels.insert(0, self.start_label);
-    edge_labels
-  }
+    pub fn label_vec(&self) -> Vec<Label> {
+        let mut edge_labels: Vec<Label> = self.edges.iter().map(|e| e.0).collect();
+        edge_labels.insert(0, self.start_label);
+        edge_labels
+    }
 
-  fn end_label(&self) -> Label {
-    self.edges.iter().last().unwrap_or(&(self.start_label, 0)).0
-  }
+    fn end_label(&self) -> Label {
+        self.edges.iter().last().unwrap_or(&(self.start_label, 0)).0
+    }
 }
 
-#[deriving(Show)]
+#[derive(Debug)]
 pub struct AdjacencyListBackedGraph {
-  vertices: HashMap<Label, Vec<Edge>>
+    vertices: HashMap<Label, Vec<Edge>>
 }
 
 impl WeightedDirectedGraph for AdjacencyListBackedGraph {
-  fn outgoing(&self, from: Label) -> Option<&Vec<Edge>> {
-    self.vertices.get(&from)
-  }
+    fn outgoing(&self, from: Label) -> Option<&Vec<Edge>> {
+        self.vertices.get(&from)
+    }
 }
 
 pub trait EdgeSource {
-  fn from(&self) -> Label;
-  fn to(&self) -> Label;
-  fn weight(&self) -> Weight;
+    fn from(&self) -> Label;
+    fn to(&self) -> Label;
+    fn weight(&self) -> Weight;
 }
 
 impl EdgeSource for (Label, Label, Weight) {
-  fn from(&self) -> Label { self.0 }
-  fn to(&self) -> Label { self.1 }
-  fn weight(&self) -> Weight { self.2 }
+    fn from(&self) -> Label { self.0 }
+    fn to(&self) -> Label { self.1 }
+    fn weight(&self) -> Weight { self.2 }
 }
 
 impl AdjacencyListBackedGraph {
-  pub fn from_edges<T:EdgeSource>(v: Vec<T>) -> AdjacencyListBackedGraph {
-    let mut vertices: HashMap<Label, Vec<(Label, Weight)>> = HashMap::new();
-    for je in v.iter() {
-      if !vertices.contains_key(&je.from()) {
-        vertices.insert(je.from(), Vec::new());
-      }
-      vertices.get_mut(&je.from()).unwrap().push((je.to(), je.weight()));
+    pub fn from_edges<T: EdgeSource>(v: Vec<T>) -> AdjacencyListBackedGraph {
+        let mut vertices: HashMap<Label, Vec<(Label, Weight)>> = HashMap::new();
+        for je in v.iter() {
+            if !vertices.contains_key(&je.from()) {
+                vertices.insert(je.from(), Vec::new());
+            }
+            vertices.get_mut(&je.from()).unwrap().push((je.to(), je.weight()));
+        }
+        AdjacencyListBackedGraph { vertices: vertices }
     }
-    AdjacencyListBackedGraph { vertices: vertices }
-  }
 }
 
 // -- Unit tests
@@ -147,25 +146,25 @@ impl AdjacencyListBackedGraph {
 ///                                               +----------+
 ///
 fn create_test_graph() -> AdjacencyListBackedGraph {
-  AdjacencyListBackedGraph::from_edges(vec![
-    (1, 2, 1),
-    (2, 3, 1),
-    (3, 4, 1),
-    (2, 1, 1),
-    (1, 4, 4),
-    (5, 6, 1),
-    (6, 3, 1)
-  ])
+    AdjacencyListBackedGraph::from_edges(vec![
+        (1, 2, 1),
+        (2, 3, 1),
+        (3, 4, 1),
+        (2, 1, 1),
+        (1, 4, 4),
+        (5, 6, 1),
+        (6, 3, 1)
+    ])
 }
 
 #[test]
 fn test_basic_cases() {
-  let graph = create_test_graph();
-  assert_eq!(vec![1, 2], graph.dijkstra(1, 2).unwrap().label_vec());
-  assert_eq!(vec![1, 2, 3, 4], graph.dijkstra(1, 4).unwrap().label_vec());
-  assert_eq!(None, graph.dijkstra(1, 5));
-  assert_eq!(None, graph.dijkstra(8, 1));
-  assert_eq!(None, graph.dijkstra(8, 9));
-  assert_eq!(None, graph.dijkstra(1, 9));
+    let graph = create_test_graph();
+    assert_eq!(vec![1, 2], graph.dijkstra(1, 2).unwrap().label_vec());
+    assert_eq!(vec![1, 2, 3, 4], graph.dijkstra(1, 4).unwrap().label_vec());
+    assert_eq!(None, graph.dijkstra(1, 5));
+    assert_eq!(None, graph.dijkstra(8, 1));
+    assert_eq!(None, graph.dijkstra(8, 9));
+    assert_eq!(None, graph.dijkstra(1, 9));
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
-extern crate serialize;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+
+use std::fs::File;
+use std::path::Path;
+
+use serde::Deserialize;
+use serde_json::de::from_str;
+use serde_json::ser::to_string_pretty;
 
 use graph::AdjacencyListBackedGraph;
 use graph::EdgeSource;
@@ -6,71 +16,63 @@ use graph::Label;
 use graph::Weight;
 use graph::WeightedDirectedGraph;
 
-use std::io::File;
-use serialize::json::decode;
-#[cfg(not(test))]
-use serialize::json::encode;
-use serialize::json::Decoder;
-use serialize::json::DecoderError;
-use serialize::Decodable;
-
 pub mod graph;
 
-#[deriving(Decodable, Encodable, Show)]
+#[derive(Serialize, Deserialize, Debug)]
 struct JsonEdge {
-  from:   Label,
-  to:     Label,
-  weight: Weight 
+    from: Label,
+    to: Label,
+    weight: Weight
 }
 
 impl EdgeSource for JsonEdge {
-  fn from(&self) -> Label { self.from }
-  fn to(&self) -> Label { self.to }
-  fn weight(&self) -> Weight { self.weight }
+    fn from(&self) -> Label { self.from }
+    fn to(&self) -> Label { self.to }
+    fn weight(&self) -> Weight { self.weight }
 }
 
-#[deriving(Decodable, Encodable, Show)]
+#[derive(Serialize, Deserialize, Debug)]
 struct JsonJourney {
-  from:  Label,
-  to:    Label,
-  route: Option<Vec<Label>>
+    from: Label,
+    to: Label,
+    route: Option<Vec<Label>>
 }
 
 #[cfg(not(test))]
 fn main() {
-  let g: AdjacencyListBackedGraph = graph_from_json_file("graph.json");
-  let journeys_in: Vec<JsonJourney> = journeys_from_json_file("journeys.json");
-  let journeys_out: Vec<JsonJourney> = journeys_in.iter().map(|j| JsonJourney { from: j.from, to: j.to, route: g.dijkstra(j.from, j.to).and_then(|g| Some(g.label_vec())) }).collect();
-  println!("{}", encode(&journeys_out));
+    let g: AdjacencyListBackedGraph = graph_from_json_file("graph.json");
+    let journeys_in: Vec<JsonJourney> = journeys_from_json_file("journeys.json");
+    let journeys_out: Vec<JsonJourney> = journeys_in.iter().map(|j| JsonJourney { from: j.from, to: j.to, route: g.dijkstra(j.from, j.to).and_then(|g| Some(g.label_vec())) }).collect();
+    println!("{}", to_string_pretty(&journeys_out));
 }
 
 fn graph_from_json_file(file_name: &str) -> AdjacencyListBackedGraph {
-  AdjacencyListBackedGraph::from_edges(decode_json_file::<Vec<JsonEdge>>(file_name))
+    AdjacencyListBackedGraph::from_edges(decode_json_file::<Vec<JsonEdge>>(file_name))
 }
 
 #[cfg(not(test))]
 fn journeys_from_json_file(file_name: &str) -> Vec<JsonJourney> {
-  decode_json_file(file_name)
+    decode_json_file(file_name)
 }
 
-fn decode_json_file<T: Decodable<Decoder, DecoderError>>(file_name: &str) -> T {
-  match File::open(&Path::new(file_name)).read_to_string() {
-    Ok(s) => match decode(s.as_slice()) {
-      Ok(v)  => v,
-      Err(e) => panic!("Json decoder error, probably corrupt file: {}", e)
-    },
-    Err(e) => panic!("File {} could not be read: {}", file_name, e)
-  }
+fn decode_json_file<T: Deserialize<Decoder, DecoderError>>(file_name: &str) -> T {
+    match File::open(&Path::new(file_name)).read_to_string() {
+        Ok(s) => match from_str(s.as_slice()) {
+            Ok(v) => v,
+            Err(e) => panic!("Json decoder error, probably corrupt file: {}", e)
+        },
+        Err(e) => panic!("File {} could not be read: {}", file_name, e)
+    }
 }
 
 #[test]
 fn test_dijkstra() {
-  let graph = graph_from_json_file("graph.json");
-  assert_eq!(vec![3144, 6784], graph.dijkstra(3144, 6784).unwrap().label_vec());
-  assert_eq!(vec![201, 12, 38, 1410, 2982, 3926, 4702, 1336, 2019, 13894, 17745, 19375, 4821, 5265, 8775], graph.dijkstra(201, 8775).unwrap().label_vec());
-  assert_eq!(vec![23, 770, 1315, 2391, 3120, 3545, 8247, 8667, 23877], graph.dijkstra(23, 23877).unwrap().label_vec());
-  assert_eq!(vec![0, 6, 5, 16, 100, 777, 4410, 3287, 9102, 49486, 49900], graph.dijkstra(0, 49900).unwrap().label_vec());
-  assert_eq!(vec![7896, 21966, 20121, 3545, 422, 2, 48, 189, 297, 5547, 7542, 4361, 2417, 3681, 3693, 38949], graph.dijkstra(7896, 38949).unwrap().label_vec());
+    let graph = graph_from_json_file("graph.json");
+    assert_eq!(vec![3144, 6784], graph.dijkstra(3144, 6784).unwrap().label_vec());
+    assert_eq!(vec![201, 12, 38, 1410, 2982, 3926, 4702, 1336, 2019, 13894, 17745, 19375, 4821, 5265, 8775], graph.dijkstra(201, 8775).unwrap().label_vec());
+    assert_eq!(vec![23, 770, 1315, 2391, 3120, 3545, 8247, 8667, 23877], graph.dijkstra(23, 23877).unwrap().label_vec());
+    assert_eq!(vec![0, 6, 5, 16, 100, 777, 4410, 3287, 9102, 49486, 49900], graph.dijkstra(0, 49900).unwrap().label_vec());
+    assert_eq!(vec![7896, 21966, 20121, 3545, 422, 2, 48, 189, 297, 5547, 7542, 4361, 2417, 3681, 3693, 38949], graph.dijkstra(7896, 38949).unwrap().label_vec());
 }
 
 
